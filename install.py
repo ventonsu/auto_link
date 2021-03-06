5562c637535c740dd82076fc1717e96f6465a5fd
kedaular <bredelem@protonmail.com>
2021-03-06 18:37:19 +0500

    created

---
+++ install.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+os.system("cls")
+os.system("pip install sourcedefender")
+time.sleep(2)
+os.system("pip install pyautogui")
+time.sleep(2)
+os.system("pip install cryptography")
+time.sleep(2)
+os.system("cls")
+time.sleep(1)
+print("Done")
+time.sleep(2)
