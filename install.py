9b9500a78ffa5e18b15e44c106f6d48f36119c5a
kedaular <bredelem@protonmail.com>
2021-03-13 23:17:44 +0500

    updated

--- install.py
+++ install.py
@@ -1,14 +1,15 @@
 import os
 import time
 
+
 os.system("cls")
+os.system("pip install opencv-python")
+time.sleep(2)
 os.system("pip install sourcedefender")
 time.sleep(2)
 os.system("pip install pyautogui")
 time.sleep(2)
 os.system("pip install cryptography")
-time.sleep(2)
-os.system("pip install opencv-python")
 time.sleep(2)
 os.system("cls")
 time.sleep(0.1)
